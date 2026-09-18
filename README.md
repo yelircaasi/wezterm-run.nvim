a820e08db5253aa213b41a7f359b24988178b36d
yelircaasi <yelircaasi@proton.me>
2026-09-18 23:16:43 +0200

    refactor (needs tests)

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # wezterm-run.nvim
 Neovim plugin that simplifies code execution and output retrieval in Wezterm, focused on integration with neighboring panes.
+
+## Running Wezterm with Development Configuration
+
+```sh
+wezterm --config-file $HOME/repos/wezterm-run.nvim/for_wezterm/wezterm-cfg-for-dev.lua
+```
--- for_wezterm/wezterm-cfg-for-dev.lua
+++ for_wezterm/wezterm-cfg-for-dev.lua
@@ -1,5 +1,8 @@
-local config = {}
-dofile("/home/isaac/repos/wezterm-run.nvim/for_wezterm/wezterm-run.lua").apply(config)
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+dofile("/home/isaac/repos/wezterm-run.nvim/for_wezterm/wezterm-run.lua").apply(wezterm, config)
+
 return config
 
 --TODO: add path identification, highlighting, and alt-n/alt-N keybinds for jumping
